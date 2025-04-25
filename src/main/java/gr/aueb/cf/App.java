630c011e876376363f6a2374aa255022d4aaf3ee
Dionysis.Vossos <dionysis.vossos@windowslive.com>
2025-04-25 21:02:07 +0300

    feat: Criteria API added

--- src/main/java/gr/aueb/cf/App.java
+++ src/main/java/gr/aueb/cf/App.java
@@ -5,11 +5,14 @@ import gr.aueb.cf.core.enums.GenderType;
 import gr.aueb.cf.model.Region;
 import gr.aueb.cf.model.Teacher;
 import gr.aueb.cf.model.TeacherMoreInfo;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
+import jakarta.persistence.*;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.Root;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class App {
 
@@ -20,21 +23,63 @@ public class App {
 
         em.getTransaction().begin();
 
-        Teacher teacher = Teacher.builder()
-                .firstname("Κώστας")
-                .lastname("Γιαννούτσος")
-                .build();
+//        Teacher teacher = Teacher.builder()
+//                .firstname("Κώστας")
+//                .lastname("Γιαννούτσος")
+//                .build();
+//
+//        TeacherMoreInfo teacherMoreInfo = TeacherMoreInfo.builder()
+//                .dateOfBirth(LocalDateTime.of(2000, 2, 1, 10, 12, 58))
+//                .gender(GenderType.MALE)
+//                .build();
+//
+//        teacher.setTeacherMoreInfo(teacherMoreInfo);
+//        Region region = em.find(Region.class, 1);
+//        teacher.addRegion(region);
+//
+//        em.persist(teacher);
 
-        TeacherMoreInfo teacherMoreInfo = TeacherMoreInfo.builder()
-                .dateOfBirth(LocalDateTime.of(2000, 2, 1, 10, 12, 58))
-                .gender(GenderType.MALE)
-                .build();
+        // merge
+//        String sql = "SELECT t FROM Teacher t WHERE t.lastname = :lastname";
+//        TypedQuery<Teacher> query = em.createQuery(sql, Teacher.class);
+//        query.setParameter("lastname", "Ανδρούτσος");
+//        Teacher teacher = query.getSingleResult();
+//        teacher.setIsActive(false);
+//        em.merge(teacher);
 
-        teacher.setTeacherMoreInfo(teacherMoreInfo);
-        Region region = em.find(Region.class, 1);
-        teacher.addRegion(region);
+//        String sql = "SELECT t FROM Teacher t WHERE t.isActive = true AND t.region.title = :regionTitle";
+//        TypedQuery<Teacher> query = em.createQuery(sql, Teacher.class);
+//        query.setParameter("regionTitle", "Θεσσαλονίκη");
+//        List<Teacher> teachers = query.getResultList();
+//        teachers.forEach(System.out::println);
 
-        em.persist(teacher);
+        // Count των teachers ανά περιοχή
+//        String sql = "SELECT r.title, COUNT(t) FROM Region r LEFT JOIN t.teachers t GROUP BY r.title";
+//        TypedQuery<Object[]> query = em.createQuery(sql, Object[].class);
+//        List<Object[]> teachersPerRegion = query.getResultList();
+//        for (Object[] row : teachersPerRegion) {
+//            for (Object item : row) {
+//                System.out.print(item + "");
+//            }
+//            System.out.println();
+//        }
+
+        // Find all active teachers in a region
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Teacher> query = cb.createQuery(Teacher.class);
+        Root<Teacher> teacher = query.from(Teacher.class);
+        Join<Teacher, Region> region = teacher.join("region");
+
+        query.select(teacher).where(
+                cb.and(
+                        cb.isTrue(teacher.get("isActive")),
+                        cb.equal(region.get("title"),"Θεσσαλονίκη")
+                        )
+                );
+
+        List<Teacher> teachers = em.createQuery(query).getResultList();
+        teachers.forEach(System.out::println);
+
         em.getTransaction().commit();
 
         em.close();
---
+++ src/main/resources/sql/teacher_more_info.sql
@@ -0,0 +1,9 @@
+use hiber7pro2db;
+
+INSERT INTO `teacher_more_info` (`id`, `date_of_birth`, `gender`)
+    VALUES  (1,'1991-01-12 09:30:00','MALE'),
+	        (2,'2000-11-10 21:10:00','FEMALE')
+	        (3,'1979-1-1 10:10:00','FEMALE');
+    
+    ALTER TABLE `teacher_more_info` AUTO_INCREMENT = 3;
+    
